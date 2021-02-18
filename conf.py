86c76066ea7f2787ae776cf85e19c6c8d604852c
Mythili-Vummaneni <79193654+Mythili-Vummaneni@users.noreply.github.com>
2021-02-18 16:50:43 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+copyright = '2020, Mythili Vummaneni'
+author = 'Mythili Vummaneni'
 
 # The full version, including alpha/beta/rc tags
 release = ''
